9cf0038113f170fa4e5dacae5f4850d093bacb46
Halil İbrahim Şener <hisener@yahoo.com>
2019-08-15 13:09:18 +0200

    Test on Junit 5

--- src/test/java/com/github/hisener/StepVerifierTest.java
+++ src/test/java/com/github/hisener/StepVerifierTest.java
@@ -1,15 +1,15 @@
 package com.github.hisener;
 
 import java.time.Duration;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-@Test(groups = "unit")
-public class StepVerifierTest {
+class StepVerifierTest {
     private static final Duration DELAY = Duration.ofSeconds(1);
 
-    public void testA() {
+    @Test
+    void testA() {
         StepVerifier.withVirtualTime(() -> Mono.just(1).delayElement(DELAY))
                 .thenAwait(DELAY)
                 .expectNext(1)
@@ -17,7 +17,8 @@ public class StepVerifierTest {
                 .verify();
     }
 
-    public void testB() {
+    @Test
+    void testB() {
         StepVerifier.create(Mono.just(1).delayElement(DELAY))
                 .thenAwait(DELAY)
                 .expectNext(1)
